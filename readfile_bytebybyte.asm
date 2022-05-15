d725b5e2546c1633343f4cd706f4120f62ba2b17
djmatlas <djmdeveloper060796@gmail.com>
2022-05-15 13:29:45 +0530

    Fix eof implementation

--- readfile_bytebybyte.asm
+++ readfile_bytebybyte.asm
@@ -2,94 +2,87 @@
 ; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 <file_name>.o -o <file_name>
 ; Run with: ./<file_name>
 
-; Reads a file byte by byte and prints it to stdout
-; WIP
+; Read a file byte by byte and print to stdout
+ 
+SECTION .data
+msg1    db      'Hello world from ASM X86 powered by NASM', 0h          ; NOTE the null terminating byte
  
 SECTION .text
 global  _start
  
 _start:
  
-    mov     eax, file_name   ; move address of msg1 to eax
-    call    read_file    ; call reveral routine
+    mov     eax, msg1
+    call    wtokenize
  
-    call    quit    ; quit program
+    call    quit
 
 ;----------------------------------------------
-
-read_file:
+; void wtokenise(String message)
+; Function to space split a string and print each word in a new line.
+wtokenize:
+    push edx
+    push ecx
+    push ebx
     push eax
-    ;push ecx
-    ;push ebx
-    ;push edx
-    ;mov edx, eax
-    mov ecx, 0
     mov ebx, eax
-    mov eax, 5
-    int 80h
-    mov [fd_in], eax
-    ;push ecx
-    mov [bytecount], dword 0h
+.nextchar:
 
-.set_seek_offset:
-    ;pop ecx
-    mov edx, 0
-    mov ecx, [bytecount]
-    mov ebx, [fd_in]
-    mov eax, 19
-    int 80h
-    ;push ecx
+    ; parse the string, if we encounter whitespace (hex 20h) then we have found the end of the current word
+    cmp byte [eax], 20h
+    jz .newword
+    cmp byte [eax], 0
+    jz .newword
+    inc eax
+    jmp .nextchar
 
-.read_byte:
-    mov edx, 1
-    mov ecx, content
-    mov ebx, [fd_in]
-    mov eax, 3
-    int 80h
+.newword:
 
-    add dword[bytecount], eax
+    ; store start and end of the previous word in stack
+    push eax    ; eax = end + 1 of the previous word
+    push ebx    ; ebx = start of the previous word
+    sub eax, ebx    ; store length of the word in eax
 
+.printword:
+    mov edx, eax    ; store length of word in edx
+    mov ecx, ebx    ; store start position (address) of the string in ecx
+    mov ebx, 1      ; 1 is std output
+    mov eax, 4      ; 4 is the syscall number for write syscall
+    int 80h         ; call syscall handler
 
-.print_byte:
-    mov edx, 1
-    mov ecx, content
-    mov ebx, 1
-    mov eax, 4
-    int 80h
+    ; print a new line
+    mov eax, 0Ah    ; mov 0Ah (new line in hex) to eax
+    push eax        ; save eax on stack
+    mov edx, 1      ; store 1 in edx, because thats the length of the string (newline) we are going to print
+    mov ecx, esp    ; move starting postion (address) of the string to ecx which is basically stack pointer
+    mov ebx, 1      ; move std out to ebx
+    mov eax, 4      ; same as before
+    int 80h         
+    
+    pop eax         ; pop eax once to get rid of the 0Ah from stack
+    pop ebx         ; restore the start of the previous word in ebx
+    pop eax         ; retsore the end of the previous word in eax
+    cmp byte[eax], 0
+    jz .finished
 
-;.print_newline:
-    ;mov eax, 0Ah
-    ;push eax
-    ;mov edx, 1
-    ;mov ecx, esp
-    ;mov ebx, 1
-    ;mov eax, 4
-    ;int 80h
+.bypass_all_whitespaces:
+    inc eax         ; increment eax to point to the beginning of the next word
+    cmp byte[eax], 20h  ; check if next byte is whitespace as well, if so we need to skip it
+    jz .bypass_all_whitespaces
+    mov ebx, eax    ; set ebx to start of the next word
+    jmp .nextchar   ; repeat the parsing process
 
-    ;pop eax
-    jmp .set_seek_offset
+.finished:
 
-.close_file:
-    mov ebx, [fd_in]
-    mov eax, 6
-    int 80h
-
-.finshed:
-    pop eax
-    pop eax
+    ; restore the early state of all the registers we used
+    pop eax         
+    pop ebx
+    pop ecx
+    pop edx
     ret
 
-; routine to quit the program
 quit:
     mov     ebx, 0
     mov     eax, 1
     int     80h
     ret
-
-SECTION .data
-file_name db 'test_file.txt'
-
-SECTION .bss
-fd_in resd 1
-content resb 1
-bytecount resd 1
